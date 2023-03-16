6b25db9ad749f75cf6245b7cddf4bcb60d50294f
Stepan <ifomstep@icloud.com>
2023-03-17 01:54:15 +0500

    Fix button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLightViews()
+        trafficButton.layer.cornerRadius = 10
     }
     
     @IBAction func trafficButtonDidTapped() {
