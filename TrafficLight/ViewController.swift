7071d0e5f59c11b96e6bb30b61174ebeadf12382
Stepan <ifomstep@icloud.com>
2023-03-17 01:15:53 +0500

    Setup UI

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var lightViews: [UIView]!
+    @IBOutlet weak var trafficButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupLightViews()
     }
-
+    
+    @IBAction func trafficButtonDidTapped() {
+        trafficButton.setTitle("NEXT", for: .normal)
+    }
+    
+    private func setupLightViews() {
+        for view in lightViews {
+            view.layer.cornerRadius = view.frame.height / 2
+            view.layer.opacity = 0.35
+        }
+        
+    }
 
 }
 
